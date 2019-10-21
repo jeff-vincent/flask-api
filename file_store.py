f55c7c57f1bd2deb99cec7f92b3fbe4fa1395a28
Jeff Vincent <jeff.d.vincent@gmail.com>
2019-10-20 19:34:08 -0700

    Expand FileStore

--- file_store.py
+++ file_store.py
@@ -1,10 +1,14 @@
-from admin import Admin
-from flask import session, jsonify
+from flask import session, jsonify, make_response
+from base64 import b64encode, b64decode, decodebytes
+import io
+import requests
+import os
+
+# Local modules
 from extensions import db
 from models import File, file_schema
 from config import Config
-import requests
-from base64 import b64encode
+from admin import Admin
 
 
 class FileStore:
@@ -28,7 +32,6 @@ class FileStore:
             else:
                 return 'Connection to Mongo failed.'
 
-
     def download(request):
         filename = request.form['filename']
         if session['logged_in']:
@@ -38,14 +41,40 @@ class FileStore:
             r = requests.post(Config.FILE_STORE_URI + 'download', data=data)
 
             if r.status_code == 200:
-                img = b64encode(r.content)
-                img = img.decode('utf-8')
-                return '<img id="'+filename+'" src="data:image/jpg;base64,'+ img +'">'
+                file_type = FileStore._get_file_type(filename)
+                if file_type in ['.jpg', '.png']:
+                    return FileStore._handle_image(r, filename)
+                if file_type =='.pdf':
+                    return FileStore._handle_pdf(r, filename)
+                else:
+                    return 'Unrecognized file type. Returning data as stored: ' + str(r.content)
+
             else:
                 return 'Connection to Mongo failed.'
+
+    def _get_file_type(filename):
+        base_name, extension = os.path.splitext(filename)
+        return extension
+
+    def _handle_image(r, filename):
+        img = b64encode(r.content)
+        img = img.decode('utf-8')
+        return '<img id="'+ filename +'" src="data:image/jpg;base64,'+ img +'">'
+
+    
+    def _handle_pdf(r, filename):
+        buffer = io.BytesIO()
+        pdf = b64encode(r.content)
+        pdf = b64decode(pdf)
+        buffer.write(pdf)
+        response = make_response(buffer.getvalue())
+        response.headers['Content-Type'] = 'application/pdf'
+        response.headers['Content-Disposition'] = 'inline; filename=' + filename
+        return response
 
     def get_current_users_files(request):
         if session['logged_in']:
             files = db.session.query(File).filter_by(user_id=Admin.USER_ID).all()
             files = file_schema.dump(files)
             return jsonify(files)
+
